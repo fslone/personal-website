b4b51db84f55727efa466acea10ec75829a6ab6b
Eric Slone <ericslone@live.com>
2015-05-05 15:43:51 -0400

    Edited article styles and added code font

--- personalsite/settings.py
+++ personalsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '#=458c@1py3#ijpa#@jt^50+n$8vdb)ebebmx^$31mt1f*s1tu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
